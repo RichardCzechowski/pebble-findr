4530c880f6f8272ef061263a68ef07a839b2e73e
Richard Tanner <dicktanner@gmail.com>
2014-11-08 08:02:29 -0800

    Server setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express()
 var request = require('request');
-
+var car;
 
 request.post('https://api-jp-t-itc.com:443/GetVehicleInfo', {form:{
   developerkey:'40548194aeaa',
@@ -11,6 +11,7 @@ request.post('https://api-jp-t-itc.com:443/GetVehicleInfo', {form:{
 
 }}, function(err, httpResponse, body){
   console.log(body);
+  car = body;
 });
 /*
 res.setHeader("Content-Type", "application/json");
@@ -21,11 +22,13 @@ if (!!req.query.callback) {
 }
 res.send(rrr)*/
 
-var server = app.listen(3000, function () {
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-  var host = server.address().address
-  var port = server.address().port
+app.get('/', function(request, response) {
+  response.send('Hello World!'+ car)
+})
 
-  console.log('Example app listening at http://%s:%s', host, port)
-
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
