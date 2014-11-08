6ca37847fcd98efd386371493f6747557c37f91d
Richard Tanner <dicktanner@gmail.com>
2014-11-08 08:16:39 -0800

    server add toyota

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express();
+var request = require('request');
+var car;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -11,3 +13,15 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+request.post('https://api-jp-t-itc.com:443/GetVehicleInfo', {form:{
+    developerkey:'40548194aeaa',
+      responseformat:'json',
+        vid:'ITCUS_VID_052',
+          infoids:'[Posn,VehBehvr]'
+
+}}, function(err, httpResponse, body){
+    console.log(body);
+      car = body;
+});
+
