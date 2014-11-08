e0312b3b81a50a5fb9514ce6f1cc47ae0a232bce
Richard Tanner <dicktanner@gmail.com>
2014-11-08 08:12:53 -0800

    New server

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-var express = require('express');
-var app = express()
-var request = require('request');
-var car;
-
-request.post('https://api-jp-t-itc.com:443/GetVehicleInfo', {form:{
-  developerkey:'40548194aeaa',
-  responseformat:'json',
-  vid:'ITCUS_VID_052',
-  infoids:'[Posn,VehBehvr]'
-
-}}, function(err, httpResponse, body){
-  console.log(body);
-  car = body;
-});
-/*
-res.setHeader("Content-Type", "application/json");
-if (!!req.query.callback) {
-  rrr = req.query.callback+"("+JSON.stringify(rtn)+");";
-} else {
-  rrr = JSON.stringify(rtn);
-}
-res.send(rrr)*/
+var express = require('express')
+var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!'+ car)
+  response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
